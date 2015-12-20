402aee99f2c268788505375eb083d621b09db64c
Jerko Steiner <jerko.steiner@gmail.com>
2015-12-20 12:37:57 -0500

    Add header and footer to app layout

---
+++ src/js/components/app.js
@@ -0,0 +1,27 @@
+const React = require('react');
+
+const Mousepad = require('./mousepad.js');
+const Input = require('./input.js');
+const Log = require('./log.js');
+const Navbar = require('./navbar.js');
+
+const navStore = require('../store/navStore.js');
+
+function app() {
+  let active = navStore.getActive();
+
+  let panels = {
+    mousepad: <Mousepad throttle="10" />,
+    logs: <Log />
+  };
+
+  let panel = panels[active];
+
+  return (<div className="app">
+    <Navbar />
+    {panel}
+    <Input />
+  </div>);
+}
+
+module.exports = app;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const Remote = require('./components/remote.js');
+const App = require('./components/app.js');
 
-ReactDom.render(<Remote />, document.querySelector('#container'));
+ReactDom.render(<App />, document.querySelector('#container'));
